bddf006eb8d933ffa2e7e869e573be1ee877cfd3
AsukFavour <favourasuk@gmail.com>
2026-05-26 12:34:29 +0100

    refactor: implement code splitting with React.lazy and Suspense in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Component/Home';
-import About from './Component/About';
-import Academics from './Component/Academics';
-import ContactUs from './Component/ContactUs';
-import Gallery from './Component/Gallery';
-import Calendar from './Component/Calendar';
-import Admission from './Component/Admission';
-import Payment from './Component/Payment';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
-
+const Home = lazy(() => import('./Component/Home'));
+const About = lazy(() => import('./Component/About'));
+const Academics = lazy(() => import('./Component/Academics'));
+const ContactUs = lazy(() => import('./Component/ContactUs'));
+const Gallery = lazy(() => import('./Component/Gallery'));
+const Calendar = lazy(() => import('./Component/Calendar'));
+const Admission = lazy(() => import('./Component/Admission'));
+const Payment = lazy(() => import('./Component/Payment'));
 
 function App() {
   return (
-    
     <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/academics' element={<Academics/>}/>
-        <Route path='/contact' element={<ContactUs/>}/>
-        <Route path='/gallery' element={<Gallery/>}/>
-        <Route path='/calendar' element={<Calendar/>}/>
-        <Route path='/admission' element={<Admission/>}/>
-        <Route path='/payment' element={<Payment/>}/>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/academics' element={<Academics/>}/>
+          <Route path='/contact' element={<ContactUs/>}/>
+          <Route path='/gallery' element={<Gallery/>}/>
+          <Route path='/calendar' element={<Calendar/>}/>
+          <Route path='/admission' element={<Admission/>}/>
+          <Route path='/payment' element={<Payment/>}/>
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
